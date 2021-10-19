17f51ab3f96b1ea4135abe25e036cc7d851d7fb7
Alexxs <irreligious86@gmail.com>
2021-10-19 22:40:06 +0200

    Updates

---
+++ precache-manifest.42f8e500dc0a983e452259fad29c9578.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe8ea37341559a81f913f527763dfcf",
+    "url": "/TodoList-React_TS/index.html"
+  },
+  {
+    "revision": "9ab22f91c5cc2ae00c9d",
+    "url": "/TodoList-React_TS/static/css/main.6f30ddfc.chunk.css"
+  },
+  {
+    "revision": "f543ebb13449f015479b",
+    "url": "/TodoList-React_TS/static/js/2.c4a9203b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/TodoList-React_TS/static/js/2.c4a9203b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ab22f91c5cc2ae00c9d",
+    "url": "/TodoList-React_TS/static/js/main.cad37c3b.chunk.js"
+  },
+  {
+    "revision": "be59a899acc752e14cc6",
+    "url": "/TodoList-React_TS/static/js/runtime-main.6cd5225b.js"
+  }
+]);
