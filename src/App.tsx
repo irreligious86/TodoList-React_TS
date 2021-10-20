83a25235b8ea62afa8c39d03e7ae87845ffca45a
Alexxs <irreligious86@gmail.com>
2021-10-20 23:05:44 +0200

    test again commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,10 @@ function App() {
         {id: 3, title: "React.JS", isDone: false},
         {id: 4, title: "TypeScript", isDone: false}
     ]
-    // const tasks2 = [
-    //     {id: 1, title: "Hello, world!", isDone: true},
-    //     {id: 2, title: "I am Happy", isDone: false},
-    //     {id: 3, title: "Yo!", isDone: false}
-    // ]
 
     return (
         <div className="App">
             <Todolist title="What to learn" tasks={tasks1}/>
-            {/*<Todolist title="Songs" tasks={tasks2}/>*/}
         </div>
     );
 }
